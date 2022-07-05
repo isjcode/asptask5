83d4cd17a303d6e6754a1d6ab3fe407f926035d2
ismayil <ismayilaj@code.edu.az>
2022-07-05 00:39:48 -0700

    das

---
+++ P225_ASP_04-06-2022_Identity_Register_Login_Logout_Authorize-main/P225Allup/P225Allup/Areas/Manage/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P225Allup.Areas.Manage.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Area("manage")]
+        [AllowAnonymous]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
